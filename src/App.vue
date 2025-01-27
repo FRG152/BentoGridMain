90ce9015283f4f029e7e5929defc79c6f85aa9cf
FRG152 <romanflavio3@hotmail.com>
2025-01-27 15:37:26 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container-grid">
+    <!-- FIVE STARTS -->
+    <div class="box-grid five-stars">
+      <h1>
+        Social Media
+        <span>10x</span> <i>Faster</i>
+        with AI
+      </h1>
+      <img
+        alt="illustration five starts"
+        src="./assets/images/illustration-five-stars.webp"
+      />
+      <p>Over 4,000 5-star reviews</p>
+    </div>
+    <!-- MULTIPLE PLATFORMS -->
+    <div class="box-grid multiple-platforms">
+      <img
+        alt="illustration multiple platforms"
+        src="./assets/images/illustration-multiple-platforms.webp"
+      />
+      <h2>Manage multiple accounts and platforms.</h2>
+    </div>
+    <!-- CONSISTENT SCHEDULE -->
+    <div class="box-grid consistent-schedule">
+      <h2>Maintain a consistent posting schedule</h2>
+      <img
+        alt="Illustration consistent schedule"
+        src="./assets/images/illustration-consistent-schedule.webp"
+      />
+    </div>
+    <!-- SCHEDULE POSTS -->
+    <div class="box-grid schedule-posts">
+      <h2>Schedule to social media.</h2>
+      <img src="./assets/images/illustration-schedule-posts.webp" alt="" />
+      <p>
+        Optimize post timings to publish content at the perfect time for your
+        audience
+      </p>
+    </div>
+    <!-- GROW FOLLOWERS -->
+    <div class="box-grid grow-followers">
+      <img
+        alt="illustration grow followers"
+        src="./assets/images/illustration-grow-followers.webp"
+      />
+      <h2>Grow followers with non-stop content.</h2>
+    </div>
+    <!-- AUDIENCE GROWTH -->
+    <div class="box-grid audience-growth">
+      <h1>>56%</h1>
+      <p>faster audience growth</p>
+      <img
+        src="./assets/images/illustration-audience-growth.webp"
+        alt=""
+        width="500"
+      />
+    </div>
+    <!-- CREATE POST -->
+    <div class="box-grid create-post">
+      <h2>Create and schedule content <i>quicker.</i></h2>
+      <img src="./assets/images/illustration-create-post.webp" alt="" />
+    </div>
+    <!-- AI CONTENT -->
+    <div class="box-grid ai-content">
+      <h2>Write your content using AI.</h2>
+      <img src="./assets/images/illustration-ai-content.webp" alt="" />
+    </div>
+  </div>
+</template>
